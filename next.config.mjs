9bafa5d27da4acf7c999c01716297b46e85be25b
Joy43 <ssjoy43@gmail.com>
2024-08-09 23:50:59 +0600

    setup product

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: "https",
+            hostname: "i.ibb.co",
+          },
+          {
+            protocol: "https",
+            hostname: "ap-southeast-2.graphassets.com",
+          },
+          {
+            protocol: "https",
+            hostname: "images.clerk.dev",
+          },
+        ],
+      },
+    typescript: {
+        ignoreBuildErrors: true,
+      },
+      eslint: {
+        ignoreDuringBuilds: true,
+      },
+};
 
 export default nextConfig;
